fcdd5e2826c95e74cb8b576b9b5b63256ada07c8
reciproco <reciproco@users.noreply.github.com>
2016-04-27 23:24:57 +0200

    add possibility of server-vars customization

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ## Set environment and executes openedX manual isntall
 ##
 export OPENEDX_RELEASE="named-release/dogwood.3"
+read -p "Customize /tmp/server-vars and press any key to start installation"
+cp ./server-vars.yml /tmp/server-vars.yml
 wget https://raw.githubusercontent.com/edx/configuration/master/util/install/ansible-bootstrap.sh -O - | sudo bash
-export EXTRA_VARS="-e@$HOME/server-vars.yml"
+export EXTRA_VARS="-e@/tmp/server-vars.yml"
 ./sandbox.sh
